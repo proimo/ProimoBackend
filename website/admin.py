19015ecc50de2f3471ab4e82020808ec8cc54b1d
Dan Percic <dan.percic@student.upt.ro>
2020-08-12 23:48:29 +0300

    Initial commit

---
+++ website/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from website.models import Announce
+
+admin.site.register(Announce)
---
+++ website/models.py
@@ -0,0 +1,17 @@
+import uuid
+
+from ckeditor_uploader.fields import RichTextUploadingField
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+# Create your models here.
+
+class Announce(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(_('Title'), max_length=200)
+    address = models.CharField(max_length=200)
+    region = models.CharField(max_length=200)
+    price = models.CharField(max_length=15)
+    surface = models.IntegerField()
+    content = RichTextUploadingField()
