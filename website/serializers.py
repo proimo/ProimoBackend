1cbfcdd5e8b9ac84430bc41907356770b43e7824
Dan Percic <dan.percic@student.upt.ro>
2020-08-14 19:39:00 +0300

    serialize images to announcements

--- website/serializers.py
+++ website/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
 
-from website.models import Announcement
+from website.models import Announcement, Setting, AnnouncementImage
 
 
-class AnnouncementSerializer(serializers.HyperlinkedModelSerializer):
+class AnnouncementImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AnnouncementImage
+        fields = '__all__'
+
+
+class AnnouncementSerializer(serializers.ModelSerializer):
+    images = AnnouncementImageSerializer(many=True, read_only=True)
+
     class Meta:
         model = Announcement
+        fields = ['id', 'title', 'region', 'price', 'surface', 'address', 'content', 'images', 'created']
+
         fields = '__all__'
