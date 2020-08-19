7fd65bdbe5af7dc646535c38c428b976bb149468
Dan Percic <dan.percic@student.upt.ro>
2020-08-19 20:44:01 +0300

    user can change own profile

--- administration/admin.py
+++ administration/admin.py
@@ -1,67 +1,55 @@
-import json
 import re
-from pprint import pprint
 
 from django.contrib import admin
+from django.contrib.admin import StackedInline, ModelAdmin, site
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin, GroupAdmin, Group as BaseGroup
-from django.http import HttpRequest
-from django.shortcuts import redirect
+from django.http import HttpResponseBadRequest
 
-import administration
-from administration.forms import ProfileForm, EditProfileForm
 from administration.models import User, UserProfile, Group, Setting
 
 
-@admin.register(Setting)
-class SettingAdmin(admin.ModelAdmin):
+def get_app_list(self, request):
+    app_dict = self._build_app_dict(request)
+    app_list = sorted(app_dict.values(), key=lambda x: x['name'].lower())
+    if request.user.is_active and request.user.is_superuser:
+        return app_list
+
+    return [j for i, j in enumerate(app_list) if not j['app_label'].__eq__('administration')]
+
+
+admin.AdminSite.get_app_list = get_app_list
+
+
+class SettingAdmin(ModelAdmin):
     list_display = ('name', 'slug', 'value', 'image')
     search_fields = ['name', 'slug', 'value']
     prepopulated_fields = {'slug': ('name',)}
 
-    # class Meta:
-    #     app_label =
 
-
-# @admin.register(UserProfile)
-# class UserProfileAdmin(admin.ModelAdmin):
-#     form = ProfileForm
-#     list_display = ('phone_number', 'image')
-#     change_form_template = 'admin/edit_profile.html'
-#
-#     def render_change_form(self, request, context, *args, **kwargs):
-#         # extra = {'lame_static_text': "something static", }
-#         # context.update(extra)
-#         return super(UserProfileAdmin, self).render_change_form(request, context, *args, **kwargs)
-class UserProfileInline(admin.StackedInline):
+class UserProfileInline(StackedInline):
     model = UserProfile
     can_delete = False
 
 
-@admin.register(User)
 class UserAdmin(BaseUserAdmin):
     inlines = (UserProfileInline,)
 
-    def get_form(self, request, *args, **kwargs):
-        form = super(UserAdmin, self).get_form(request, *args, **kwargs)
-        return form
-
     def get_fieldsets(self, request, obj=None):
         if request.user.is_active and request.user.is_superuser:
-            fieldsets = super(UserAdmin, self).get_fieldsets(request, obj)
-            print(fieldsets)
-            return fieldsets
+            return super(UserAdmin, self).get_fieldsets(request, obj)
 
-        user_id = re.findall(r'user/(\d+)/change', str(request))[0]
-        if user_id is not str(request.user.id):
-            redirect('/api/admin')
-        print(user_id)
+        try:
+            user_id = re.findall(r'user/(\d+)/change', str(request))[0]
+        except IndexError:
+            raise HttpResponseBadRequest("You ran into troubles, gangsta!")
+        if not str(request.user.id).__eq__(user_id):
+            raise HttpResponseBadRequest("You're not allowed to do that, bad boy!")
+
         return [(None, {'fields': ('username', 'password',)}),
                 ('Personal info', {'fields': ('first_name', 'last_name', 'email')}), ]
 
 
-class PLM(BaseUserAdmin):
-    inlines = (UserProfileInline,)
-
-
-admin.site.unregister(BaseGroup)
-admin.site.register(Group, GroupAdmin)
+site.unregister(BaseGroup)
+site.register(Group, GroupAdmin)
+site.register(Setting, SettingAdmin)
+site.register(User, UserAdmin)
