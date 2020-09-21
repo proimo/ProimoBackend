c5dd862bdf141168814903584501525f65e11efe
Dan Percic <dan.percic@student.upt.ro>
2020-09-21 13:40:32 +0300

    add fieldsets to apartment sell admin

--- offers/sales/admin.py
+++ offers/sales/admin.py
@@ -101,6 +101,48 @@ class ApartmentSaleAdmin(SaleOfferAdmin):
     internet_fieldsets = (
         'Acces internet', {'fields': (('has_wired_net', 'has_fiber', 'has_wireless', 'has_dial_up'),)})
     interior_state_fieldsets = ('Stare interior', {'fields': (('is_renovated', 'is_good', 'need_renovation'),)})
+    windows_fieldsets = (
+        'Ferestre cu geam termopan', {'fields': (('has_aluminium_windows', 'has_wood_windows', 'has_pvc_windows'),)})
+    louver_fieldsets = ('Jaluzele', {'fields': (('has_horizontal_louver', 'has_vertical_louver'),)})
+    rolls_fieldsets = ('Rulouri / obloane', {'fields': (('has_pvc_rolls', 'has_wood_rolls', 'has_aluminium_rolls'),)})
+    entrance_door_fieldsets = ('Uşă intrare', {'fields': (('has_pal_entrance_door', 'has_wood_entrance_door',
+                                                           'has_metal_entrance_door', 'has_pvc_entrance_door',
+                                                           'has_parquet_entrance_door'),)})
+    heat_isolation_fieldsets = (
+        'Izolaţii termice', {'fields': (('has_indoor_heat_isolation', 'has_outdoor_heat_isolation'),)})
+    floor_fieldsets = ('Podele', {'fields': (('has_linoleum_floor', 'has_carpet_floor', 'has_parquet_floor',
+                                              'has_tiles_floor', 'has_decking_floor', 'has_marble_floor'),)})
+    interior_doors_fieldsets = ('Uşi interior', {'fields': (('has_cellular_interior_door', 'has_wood_interior_door',
+                                                             'has_panel_interior_door', 'has_pvc_interior_door',
+                                                             'has_glass_interior_door'),)})
+    walls_fieldsets = ('Pereţi', {'fields': (('has_chalk_walls', 'has_vinarom_walls', 'has_washable_paint_walls',
+                                              'has_faience_walls', 'has_wainscot_walls', 'has_wallpaper_walls',
+                                              'has_clay_walls'),)})
+    other_util_spaces_fieldsets = (
+        'Alte spaţii utile', {'fields': (('has_terrace', 'has_service_wc', 'has_basement_box', 'has_storage_closet'),)})
+    kitchen_fieldsets = ('Bucătărie', {'fields': (
+        ('has_furnished_kitchen', 'has_half_furnished_kitchen', 'has_equipped_kitchen', 'has_half_equipped_kitchen'),)})
+    counters_fieldsets = ('Contorizare', {'fields': (('has_gas_counter', 'has_water_counter', 'has_heat_counter'),)})
+    furnished_fieldsets = (
+        'Mobilat', {'fields': (('is_not_furnished', 'is_half_furnished', 'is_full_furnished', 'is_lux_furnished'),)})
+    appliances_fieldsets = ('Electrocasnice', {'fields': (('has_iron', 'has_dishwasher', 'has_coffee_maker',
+                                                           'has_wash_machine', 'has_toaster', 'has_fridge', 'has_oven',
+                                                           'has_gas_cooker', 'has_hood', 'has_kitchen_robot',
+                                                           'has_hairdryer', 'has_sandwich_maker', 'has_hi_fi', 'has_tv',
+                                                           'has_vacuum_cleaner', 'has_dvd'),)})
+    diverse_fieldsets = ('Diverse', {'fields': (('has_smoke_sensor', 'has_alarm_system', 'has_fireplace',
+                                                 'has_jacuzzi',
+                                                 'has_garage_remote', 'has_auto_access_remote',
+                                                 'has_interior_stairway'),)})
+    building_features_fieldsets = ('Dotări imobil', {'fields': (('has_recreation_spaces', 'has_video_intercom',
+                                                                 'has_interior_pool', 'has_sauna', 'has_roof',
+                                                                 'has_yard', 'has_common_yard', 'has_garden',
+                                                                 'has_intercom', 'has_elevator', 'has_exterior_pool',
+                                                                 'has_dryer', 'has_spa'),)})
+    building_services_fieldsets = ('Servicii imobil', {'fields': (
+        ('has_administration', 'has_housekeeping', 'has_security', 'has_video_security'),)})
+    hotel_services_fieldsets = ('Servicii hoteliere', {
+        'fields': (('has_cleaning', 'has_bed_sheets', 'has_towels', 'has_station_transfer', 'has_city_tour'),)})
 
     readonly_fields = ['has_ground_floor']
     fieldsets = (
@@ -120,6 +162,23 @@ class ApartmentSaleAdmin(SaleOfferAdmin):
         climate_fieldsets,
         internet_fieldsets,
         interior_state_fieldsets,
+        windows_fieldsets,
+        louver_fieldsets,
+        rolls_fieldsets,
+        entrance_door_fieldsets,
+        heat_isolation_fieldsets,
+        floor_fieldsets,
+        interior_doors_fieldsets,
+        walls_fieldsets,
+        other_util_spaces_fieldsets,
+        kitchen_fieldsets,
+        counters_fieldsets,
+        furnished_fieldsets,
+        appliances_fieldsets,
+        diverse_fieldsets,
+        building_features_fieldsets,
+        building_services_fieldsets,
+        hotel_services_fieldsets,
         SaleOfferAdmin.time_fieldsets
     )
 
